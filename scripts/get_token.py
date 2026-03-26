60f758c05eb87dcc51702e80a40ad0c63ab478ac
Padmanabh Singh <ps4015@hw.ac.uk>
2026-03-27 06:55:45 +0800

    Added image , changed png to jpg in routers specialists endpoints

--- scripts/get_token.py
+++ scripts/get_token.py
@@ -9,7 +9,7 @@ key = os.environ["SUPABASE_ANON_KEY"]
 
 supabase = create_client(url, key)
 
-email =    "provider1@demo.com" #"testuser1@demo.com" 
+email =   "testuser1@demo.com"  #"provider1@demo.com" 
 password = "12345678"
 
 res = supabase.auth.sign_in_with_password({
